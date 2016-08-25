c92abf244f2960a3358ba3debf56bc8d89dc2a70
Rafał Łasocha <me+dev@swistak35.com>
2016-08-26 01:42:33 +0200

    added

---
+++ fluxbox/scripts/startup.after.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#sleep 15
+#xrandr --output LVDS1 --mode 1280x800 &
+#session.screen0.rootCommand:	fbsetbg -l
+~/.fluxbox/scripts/resolution.sh &
+conky -c ~/.conky/conkyhr &
+#bmpanel KrystalXP16BlackGloss-mod &
+#fbpanel &
+#yeahconsole &
+pidgin &
+#geany &
+#filezilla &
+#liferea --mainwindow-state=hidden &
+icedove &
+#exaile &
+#rhythmbox &
+opera &
+#iceweasel &
+wicd-gtk &
+#xchat --minimize=2
+#dropbox &
+fbsetbg -l &
+#/home/swistak35/programs/ST2/sublime_text &
+#xscreensaver &
+#blueman-manager
+#truecrypt --auto-mount=favorites &
+#gwibber &
