607f46d8a1a96462541854c174d983baf1cbc39e
Rafał Łasocha <me+dev@swistak35.com>
2016-08-26 01:31:26 +0200

    up

--- zsh/promptleft.zsh
+++ zsh/promptleft.zsh
@@ -5,5 +5,6 @@ if [[ -a ~/.promptcolor ]]; then
 	PROMPTCOLOR=`cat ~/.promptcolor`
 else
   	PROMPTCOLOR=blue
+	PROMPTCOLOR_STATUSCODE=red
 fi
-PROMPT="%{$fg_bold[$PROMPTCOLOR]%}%~%# %{$reset_color%}"
+PROMPT="%(?..%{$fg_bold[$PROMPTCOLOR_STATUSCODE]%}[%?]%{$reset_color%})%{$fg_bold[$PROMPTCOLOR]%}%~%# %{$reset_color%}"
