94b540c232ad35bd848b4923db647d7a3766d8d7
Rafał Łasocha <me@swistak35.com>
2017-10-24 18:59:14 +0200

    up

---
+++ bin/scripts/diskspace.sh
@@ -0,0 +1,14 @@
+cd /home/swistak35
+echo "home dotfiles"
+\du .* | sort -n | tail
+echo
+
+cd /home/swistak35/Pobrane
+echo "downloads"
+\du * | sort -n | tail
+echo
+
+cd /home/swistak35/.cache
+echo "cache"
+\du * | sort -n | tail
+echo
