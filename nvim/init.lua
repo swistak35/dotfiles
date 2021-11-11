ce96452e2195a3e744f8ba4b4521bc4265d8362d
Rafał Łasocha <me@swistak35.com>
2021-11-11 16:01:03 +0100

    Basic lua neovim config

---
+++ nvim/init.lua
@@ -0,0 +1,22 @@
+-- Install packer
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+end
+
+vim.api.nvim_exec(
+  [[
+  augroup Packer
+    autocmd!
+    autocmd BufWritePost init.lua PackerCompile
+  augroup end
+]],
+  false
+)
+
+local use = require('packer').use
+require('packer').startup(function()
+  -- Package manager
+  use 'wbthomason/packer.nvim'
+end)
