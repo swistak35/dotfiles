2751835608c8e9b469e8384508f5b80374816bb6
Rafał Łasocha <rafal@lasocha.net>
2022-01-14 17:34:20 +0100

    Upgrade org-mode config

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -177,6 +177,7 @@
 
 (use-package org
              :config
+             (setq org-agenda-files '("~/notes" "~/projs/silverfin" "~/projs/railseventstore"))
              (global-set-key (kbd "C-c l") #'org-store-link)
              (global-set-key (kbd "C-c a") #'org-agenda)
              (global-set-key (kbd "C-c c") #'org-capture)
@@ -214,7 +215,6 @@
  '(custom-safe-themes
    '("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default))
  '(helm-minibuffer-history-key "M-p")
- '(org-agenda-files '("~/projs/silverfin/todo.org"))
  '(package-selected-packages
    '(list-packages-ext solarized-theme dired-subtree dired-collapse dired-hacks-utils)))
 (custom-set-faces
