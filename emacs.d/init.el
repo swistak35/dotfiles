963ec2f66df993f7ee6c6e9855a59a22b6d035f1
Rafał Łasocha <rafal@lasocha.net>
2022-01-17 08:17:40 +0100

    Ensure that CLOSED timestamps will be added to tasks

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -178,6 +178,7 @@
 (use-package org
              :config
              (setq org-agenda-files '("~/notes" "~/projs/silverfin" "~/projs/railseventstore"))
+             (setq org-log-done t)
              (global-set-key (kbd "C-c l") #'org-store-link)
              (global-set-key (kbd "C-c a") #'org-agenda)
              (global-set-key (kbd "C-c c") #'org-capture)
