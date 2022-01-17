cbc80339bda9b4a3463e895a7743d28ef44c62c9
Rafał Łasocha <rafal@lasocha.net>
2022-01-17 08:17:58 +0100

    Don't display DONE SCHEDULED items in the agenda

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -179,6 +179,7 @@
              :config
              (setq org-agenda-files '("~/notes" "~/projs/silverfin" "~/projs/railseventstore"))
              (setq org-log-done t)
+             (setq org-agenda-skip-scheduled-if-done t)
              (global-set-key (kbd "C-c l") #'org-store-link)
              (global-set-key (kbd "C-c a") #'org-agenda)
              (global-set-key (kbd "C-c c") #'org-capture)
