9cf435b4f47cb38f6b98e18dfc083ebdeea020bc
Rafał Łasocha <rafal@lasocha.net>
2025-08-09 01:13:33 +0200

    [emacs] Improvements to movies integration

--- emacs.d/lisp/emacs-movies.el
+++ emacs.d/lisp/emacs-movies.el
@@ -17,6 +17,161 @@
   '("netflix" "disney" "viaplay" "skyshowtime" "canalplus" "cineman" "appletv" "hbomax" "cdapremium" "amazon" "tvpvod")
   "List of supported streaming service subscriptions.")
 
+(defvar emacs-movies-directory "/media/plex/Wideo"
+  "Directory containing video files.")
+
+(defvar emacs-movies-video-extensions '("mkv" "mp4" "avi")
+  "List of video file extensions to search for.")
+
+(defun emacs-movies-all-video-files ()
+  "Return a list of all video files in `emacs-movies-directory'.
+Raises an error if the variable is not set or directory does not exist."
+  (unless emacs-movies-directory
+    (error "emacs-movies-directory is not set"))
+  (unless (file-exists-p emacs-movies-directory)
+    (error "Directory does not exist: %s" emacs-movies-directory))
+  (unless (file-directory-p emacs-movies-directory)
+    (error "Path is not a directory: %s" emacs-movies-directory))
+
+  (let ((video-files '())
+        (extensions-regex (concat "\\." (regexp-opt emacs-movies-video-extensions) "$")))
+    (dolist (file (directory-files-recursively emacs-movies-directory extensions-regex))
+      (when (file-regular-p file)
+        (push file video-files)))
+    (nreverse video-files)))
+
+(defun extract-tmdb-id-from-filepath (filepath)
+  "Extract TMDB ID from filepath if it contains a tmdb tag.
+The tmdb tag format is {tmdb-TMDB_ID} where TMDB_ID are only numbers.
+Returns the TMDB_ID as a string if found, nil otherwise."
+  (let ((filename (file-name-nondirectory filepath)))
+    (when (string-match "{tmdb-\\([0-9]+\\)}" filename)
+      (match-string 1 filename))))
+
+(defun filter-files-without-tmdb-tag (files)
+  "Filter FILES list to return only files that do NOT have a TMDB tag.
+Uses `extract-tmdb-id-from-filepath' to check for TMDB tags."
+  (cl-remove-if (lambda (file)
+                  (extract-tmdb-id-from-filepath file))
+                files))
+
+(defun filter-files-with-tmdb-tag (files)
+  "Filter FILES list and return mapping from TMDB_ID to list of filepaths.
+Returns an alist where each element is (TMDB_ID . (list of filepaths)).
+Uses `extract-tmdb-id-from-filepath' to extract TMDB IDs."
+  (let ((tmdb-map '()))
+    (dolist (file files)
+      (let ((tmdb-id (extract-tmdb-id-from-filepath file)))
+        (when tmdb-id
+          (let ((existing (assoc tmdb-id tmdb-map)))
+            (if existing
+                (setcdr existing (cons file (cdr existing)))
+              (push (cons tmdb-id (list file)) tmdb-map))))))
+    tmdb-map))
+
+(defun deobfuscate-filepath (filepath)
+  "Deobfuscate filename by removing codec/quality information for comparison.
+Takes only the filename into account and removes technical metadata."
+  (let* ((filename (file-name-nondirectory filepath))
+         (name-without-ext (file-name-sans-extension filename))
+         (deobfuscated name-without-ext))
+
+    ;; Remove common quality indicators
+    (setq deobfuscated (replace-regexp-in-string "\\b\\(720p\\|1080p\\|1440p\\|2160p\\|4K\\|HD\\|UHD\\|FHD\\)\\b" "" deobfuscated t t))
+
+    ;; Remove common codec information
+    (setq deobfuscated (replace-regexp-in-string "\\b\\(x264\\|x265\\|h264\\|h265\\|HEVC\\|AVC\\|XviD\\|DivX\\)\\b" "" deobfuscated t t))
+
+    ;; Remove common source/release info
+    (setq deobfuscated (replace-regexp-in-string "\\b\\(BluRay\\|BRRip\\|DVDRip\\|WEBRip\\|HDTV\\|WEB-DL\\|BDRip\\)\\b" "" deobfuscated t t))
+
+    ;; Remove common audio codecs
+    (setq deobfuscated (replace-regexp-in-string "\\b\\(AAC\\|AC3\\|DTS\\|MP3\\|FLAC\\)\\b" "" deobfuscated t t))
+
+    ;; Remove release group info (usually at the end in brackets or after dash)
+    (setq deobfuscated (replace-regexp-in-string "[-\\[].*$" "" deobfuscated))
+
+    ;; Remove TMDB tags
+    (setq deobfuscated (replace-regexp-in-string "{tmdb-[0-9]+}" "" deobfuscated))
+
+    ;; Remove multiple spaces and trim
+    (setq deobfuscated (replace-regexp-in-string "\\s-+" " " deobfuscated))
+    (setq deobfuscated (string-trim deobfuscated))
+
+    deobfuscated))
+
+(defun edit-distance (str1 str2)
+  "Compute the Levenshtein edit distance between STR1 and STR2.
+Returns the minimum number of single-character edits (insertions, deletions,
+or substitutions) required to change one string into the other."
+  (let* ((len1 (length str1))
+         (len2 (length str2))
+         (matrix (make-vector (1+ len1) nil)))
+
+    ;; Initialize matrix
+    (dotimes (i (1+ len1))
+      (aset matrix i (make-vector (1+ len2) 0)))
+
+    ;; Fill first row and column
+    (dotimes (i (1+ len1))
+      (aset (aref matrix i) 0 i))
+    (dotimes (j (1+ len2))
+      (aset (aref matrix 0) j j))
+
+    ;; Fill the matrix
+    (dotimes (i len1)
+      (dotimes (j len2)
+        (let* ((row (1+ i))
+               (col (1+ j))
+               (cost (if (eq (aref str1 i) (aref str2 j)) 0 1))
+               (deletion (1+ (aref (aref matrix i) col)))
+               (insertion (1+ (aref (aref matrix row) j)))
+               (substitution (+ (aref (aref matrix i) j) cost)))
+          (aset (aref matrix row) col
+                (min deletion insertion substitution)))))
+
+    ;; Return the edit distance
+    (aref (aref matrix len1) len2)))
+
+(defun find-files-with-tmdb-id (tmdb-id files)
+  "Find files with specific TMDB_ID tag from a list of FILES.
+Returns a list of filepaths that contain {tmdb-TMDB_ID} tag matching the given ID.
+Uses `filter-files-with-tmdb-tag' to group files by TMDB ID."
+  (let ((tmdb-map (filter-files-with-tmdb-tag files)))
+    (cdr (assoc tmdb-id tmdb-map))))
+
+(defun emacs-movies-find-downloaded-file ()
+  "Find downloaded file for current org entry using TMDB_URL.
+Searches for files with matching TMDB ID and asks user for confirmation.
+If confirmed, stores the filepath in DOWNLOADED_FILEPATH property."
+  (interactive)
+  (let ((tmdb-url (org-entry-get nil "TMDB_URL")))
+    (unless tmdb-url
+      (error "No TMDB_URL property found for this entry"))
+
+    (let* ((tmdb-info (extract-tmdb-id tmdb-url))
+           (tmdb-id (car tmdb-info)))
+      (unless tmdb-id
+        (error "Could not extract TMDB ID from URL: %s" tmdb-url))
+
+      (let* ((all-files (emacs-movies-all-video-files))
+             (files-with-tmdb (filter-files-with-tmdb-tag all-files))
+             (matching-files (find-files-with-tmdb-id tmdb-id all-files)))
+
+        (if matching-files
+            (let ((file-list (mapconcat (lambda (file) 
+                                          (format "  %s" file))
+                                        matching-files "\n")))
+              (when (yes-or-no-p (format "Found files:\n%s\n\nAre these the correct files for this entry? " file-list))
+                (let* ((filepath (if (= (length matching-files) 1)
+                                     (car matching-files)
+                                   (completing-read "Select file: " matching-files)))
+                       (filename (file-name-nondirectory filepath))
+                       (org-link (format "[[file:%s][%s]]" filepath filename)))
+                  (org-set-property "DOWNLOADED_FILEPATH" org-link)
+                  (message "Set DOWNLOADED_FILEPATH to: %s" org-link))))
+          (message "No downloaded files found with TMDB ID %s" tmdb-id))))))
+
 (defun extract-tmdb-id (url)
   "Extract TMDB ID and type from a TMDB URL.
 For movies like 'https://www.themoviedb.org/movie/69315-battlestar-galactica-razor'
