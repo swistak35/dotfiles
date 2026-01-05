c534bc26cfc3cdd91165861ee60aea26c60d439c
Rafał Łasocha <rafal@lasocha.net>
2026-01-05 09:08:17 +0100

    Specify treesitters versions for current emacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -109,21 +109,22 @@
 
 ; Treesitter
 (setq treesit-language-source-alist
-   '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+   '((bash "https://github.com/tree-sitter/tree-sitter-bash" "v0.23.3")
      (cmake "https://github.com/uyha/tree-sitter-cmake")
-     (css "https://github.com/tree-sitter/tree-sitter-css")
-     (elisp "https://github.com/Wilfred/tree-sitter-elisp")
-     (go "https://github.com/tree-sitter/tree-sitter-go")
+     (css "https://github.com/tree-sitter/tree-sitter-css" "v0.23.2")
+     (elisp "https://github.com/Wilfred/tree-sitter-elisp" "1.5.0")
+     (go "https://github.com/tree-sitter/tree-sitter-go" "v0.23.4")
      (html "https://github.com/tree-sitter/tree-sitter-html")
-     (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
+     (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "v0.21.4" "src")
      (json "https://github.com/tree-sitter/tree-sitter-json")
      (make "https://github.com/alemuller/tree-sitter-make")
      (markdown "https://github.com/ikatyang/tree-sitter-markdown")
-     (python "https://github.com/tree-sitter/tree-sitter-python")
+     (python "https://github.com/tree-sitter/tree-sitter-python" "v0.23.6")
      (ruby "https://github.com/tree-sitter/tree-sitter-ruby")
      (toml "https://github.com/tree-sitter/tree-sitter-toml")
      (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
      (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
+     (vue "https://github.com/ikatyang/tree-sitter-vue")
      (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
 (setq major-mode-remap-alist
  '((yaml-mode . yaml-ts-mode)
